047712c006f76b314d76ef82eabc814e08095e3d
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-19 13:49:25 -0500

    removed duplicate files

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,44 @@
 // @TODO: YOUR CODE HERE!
+
+//create svg 
+var svgArea = d3.select("body").select("svg");
+if (!svgArea.empty()) {
+svgArea.remove();
+}
+// svg params
+var svgHeight = window.innerHeight;
+var svgWidth = window.innerWidth;
+// margins
+var margin = {
+top: 50,
+right: 50,
+bottom: 50,
+left: 50
+};
+// chart area minus margins
+var chartHeight = svgHeight - margin.top - margin.bottom;
+var chartWidth = svgWidth - margin.left - margin.right;
+
+
+
+
+d3.csv("../data/data.csv").then(function(healthData) {
+    var poverty = healthData.poverty
+    var povertyMoe = healthData.povertyMoe
+    var age = healthData.age
+    var ageMoe = healthData.ageMoe
+    var income = healthData.income
+    var incomeMoe = healthData.incomeMoe
+    console.log(poverty)
+    console.log(povertyMoe)
+    console.log(age)
+    console.log(ageMoe)
+    console.log(income)
+    console.log(incomeMoe)
+    // healthcare,healthcareLow,healthcareHigh
+    // obesity,obesityLow,obesityHigh
+    // smokes,smokesLow,smokesHigh
+
+
+
+});
