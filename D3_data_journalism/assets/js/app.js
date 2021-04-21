4ddb9af76349333ecafa4b3b4ed024d7168dea9e
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-20 19:38:04 -0500

    Added axis lines

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,8 +1,8 @@
 // @TODO: YOUR CODE HERE
 
 // svg params
-var svgHeight = 800//window.innerHeight;
-var svgWidth = 800//window.innerWidth;
+var svgHeight = window.innerHeight-100;
+var svgWidth = window.innerWidth-100;
 // margins
 var margin = {
 top: 50,
@@ -15,15 +15,17 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 var chartWidth = svgWidth - margin.left - margin.right;
 
 //create svg 
-var svgArea = d3.select("body").select("#scatter");
+var svgArea = d3.select("#scatter").append("svg")
+.attr("height", svgHeight)
+.attr("width", svgWidth)
 
 // if (!svgArea.empty()) {
 //     svgArea.remove();
 // };
 //append chart
 var chartGroup= svgArea.append("g")
-.attr("width", svgWidth)
-.attr("height", svgHeight)
+.attr("width", chartWidth)
+.attr("height", chartHeight)
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
@@ -41,29 +43,32 @@ d3.csv("./assets/data/data.csv").then(function(healthData) {
     var povertyXScale = d3.scaleLinear()
     .domain(d3.extent(healthData, d =>d.poverty))
     .range([0 , chartWidth]);
-    console.log(povertyXScale)
+    
 
     var ageYScale = d3.scaleLinear()
     .domain([0,d3.max(healthData, d => d.age)])
     .range([chartHeight, 0]);
-    console.log(ageYScale)
+
+    console.log(d3.max(healthData, d=> d.poverty))
+    console.log(d3.extent(healthData, d=> d.age))
 
     var bottomAxis = d3.axisBottom(povertyXScale);
     var leftAxis = d3.axisLeft(ageYScale);
+
+    console.log(chartHeight)
+    console.log(chartWidth)
 
     // Add bottomAxis
     chartGroup.append("g").attr("transform", `translate(0, ${chartHeight})`).call(bottomAxis);
     // Add leftAxis to the left side of the display
     chartGroup.append("g").call(leftAxis);
-    // Add rightAxis to the right side of the display
-    chartGroup.append("g").attr("transform", `translate(${chartWidth}, 0)`).call(leftAxis);
+    // // Add rightAxis to the right side of the display
+    // chartGroup.append("g").attr("transform", `translate(${chartWidth}, 0)`).call(povertyXScale);
 
     
   
     // healthcare,healthcareLow,healthcareHigh
     // obesity,obesityLow,obesityHigh
     // smokes,smokesLow,smokesHigh
-
-
 
 });
